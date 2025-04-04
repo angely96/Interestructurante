5028cd670bde2864c12b57ba0116e054a6b6c9fc
Aracelly <angelyaracelly321@gmail.com>
2025-04-03 21:44:33 -0500

    Signup.php :Conexion bd

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+ $host = "localhost";
+ $port = "5432";
+ $dbname = "pro_inter";
+ $user= "";
+ $password="postgres";
+
+$conn = pg__connect("
+    host=$host
+    port= $port
+    dbname=$name
+    user=$user
+    password =$password
+
+")
+
+if(!$conn){
+    die("Connection error".preg_last_error())
+}else{
+    echo"Success connetion"
+}
+
+?>
+
